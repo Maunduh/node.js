2dab12f95543690f454d0a2e305d3658d859b7ce
amosmaundundu <amosmaundu5@gmail.com>
2023-02-08 15:53:53 +0300

    first commit

---
+++ App.js
@@ -0,0 +1,27 @@
+//global objects in javascript and node
+console.log() 
+
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+//the above global objects can be called through window in javascript engine.
+// for example window.console.log
+
+var message = ""; // also availabe in window
+//we can call it window.message.
+
+//in node.js window object is unavailable.
+//instead we have global 
+// the following functions can be accessed through global
+console.log() 
+
+setTimeout();
+clearTimeout();
+
+setInterval();
+clearInterval();
+
+//variables in node.js are not available in the the global scope
